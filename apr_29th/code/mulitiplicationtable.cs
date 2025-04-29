adce1ad7e4a1d71efb05aa5faaf2e5ff4546dfb2
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-29 17:04:35 +0530

    rint a multiplication table (like 5 x 1 = 5 ... 5 x IO = 50) using a for loop

---
+++ apr_29th/code/mulitiplicationtable.cs
@@ -0,0 +1,13 @@
+//Print a multiplication table (like 5 x 1 = 5 ... 5 x IO = 50) using a for loop
+using System;
+
+ public class HelloWorld
+{
+    static void Main()
+    {
+        for(int i=1;i<=10;i++)
+        {
+            Console.WriteLine(" 5 x " +i + " = "+ 5*i);
+        }
+    }
+}
