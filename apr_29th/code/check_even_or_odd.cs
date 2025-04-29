dedbb98a56dd232952ca09d4d101eaf6c2ace551
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-29 16:09:39 +0530

    3rd code

---
+++ apr_29th/code/check_even_or_odd.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+ public class HelloWorld
+{
+    static void Main()
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        if(num%2==0)
+        {
+            Console.WriteLine(num + " Is Even");
+        }
+        else{
+            Console.WriteLine(num + " Is Odd");
+        }
+    }
+}
