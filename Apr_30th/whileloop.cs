12b156222ac832d8226397ebb1174357ea1b879b
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-30 09:44:15 +0530

    demonstrates a simple while loop

---
+++ Apr_30th/whileloop.cs
@@ -0,0 +1,16 @@
+// This program demonstrates a simple while loop in C# that counts down from 10 to 0 and prints each number to the console.
+using System;
+
+public class helloworld
+{
+    public static void Main(string[] args)
+    {
+        int i =10;
+        
+        while(i >=0)
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+    }
+}
