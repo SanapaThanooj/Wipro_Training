65720a4f36f7e0135162e756da81d1c850276b2a
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-30 09:17:49 +0530

    switch_case_code

---
+++ Apr_30th/switchcase_code.cs
@@ -0,0 +1,27 @@
+
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int day = 4;
+        switch(day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            default:
+                Console.WriteLine("INVALID");
+                break;        
+        }
+        
+    }
+}
