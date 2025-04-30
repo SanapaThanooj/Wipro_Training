7e7a4fc32edf7ad952602ed0faeb39c02e04438f
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-30 09:52:46 +0530

    demonstrate the use of do-while loop

---
+++ Apr_30th/dowhileloop.cs
@@ -0,0 +1,24 @@
+// C# program to demonstrate the use of do-while loop
+// This program prints numbers from 10 to 6 using a do-while loop. The loop will execute at least once, even if the condition is false at the start.
+using System;
+
+public class helloworld
+{
+    public static void Main(string[] args)
+    {
+        // int i =1;
+        // do
+        // {
+        //     Console.WriteLine(i);
+        //i++;
+        //     i++;
+        // }while(i <=5);
+        int i =10;
+         do{
+             Console.WriteLine(i);
+             i++;
+         }while(i<=6);
+        
+    }
+    
+}
