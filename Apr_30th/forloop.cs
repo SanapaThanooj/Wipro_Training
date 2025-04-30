0201d51e65980b134d1c2abdf604f8e77ed9d16b
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-30 09:25:38 +0530

    for demonstrateion of for loop

---
+++ Apr_30th/forloop.cs
@@ -0,0 +1,13 @@
+\\ for demonstrateion of for loop
+using System;
+public class helloworld
+{
+    public static void Main(string[] args)
+    {
+        
+        for(int i =1;i<=5;i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
