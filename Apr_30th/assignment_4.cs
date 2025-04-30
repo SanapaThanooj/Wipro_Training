ee736d3708c4281727946985fecf4c893c1e0018
Thanooj Prapulla <thanoojprapulla@wipro>
2025-04-30 10:53:24 +0530

    Print squares of num untill the square exceeds 100

---
+++ Apr_30th/assignment_4.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+
+        int i = 1;
+        // while (i * i <= 100)
+        // {
+        //     Console.WriteLine("Square of " + i + " is " + (i * i));
+        //     i++;
+        // } 
+        
+        for (i = 1; i<= 10; i++)
+        {
+        
+            Console.WriteLine("squares of "+i+" is "+i*i);
+        }
+        
+        
+    }
+}
