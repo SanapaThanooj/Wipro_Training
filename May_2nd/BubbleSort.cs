0be36f72de1789e971df7ba540fa27e1f3014dda
Thanooj Prapulla <thanoojprapulla@wipro>
2025-05-02 10:59:03 +0530

    BubbleSort

---
+++ May_2nd/BubbleSort.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Bubblesort
+{
+    static void BubbleSort(int[] arr)
+    {
+        for(int i=0;i<arr.Length-1;i++)
+        {
+            for(int j=0;j<arr.Length-1;i++){
+               if(arr[j]>arr[j+1])
+               {
+                   int temp =arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j + 1] = temp;
+               }
+            }    
+        }
+    }
+    static void  PrintArray(int[] arr)
+    {
+        foreach(int num in arr)
+        {
+            Console.WriteLine(num+" ");
+        }
+        Console.WriteLine();
+    }
+    static void Main()
+    {
+        int [] num ={10,25,30,45,60,75};
+        Console.WriteLine("Original array");
+        PrintArray(num);
+        BubbleSort(num);
+        Console.WriteLine("Sorted array");
+        PrintArray(num);
+    }
+}
