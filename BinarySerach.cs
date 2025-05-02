30c88074b0783aafd31594adbd8390675bb81b6f
Thanooj Prapulla <thanoojprapulla@wipro>
2025-05-02 10:11:30 +0530

    Binary_search

---
+++ BinarySerach.cs
@@ -0,0 +1,37 @@
+using System;
+
+class BinarySearch
+{
+    static int BinarySearch(int[] arr, int tg)
+    {
+        int low =0;
+        int high =arr.Lenght-1;
+        whike(low <=high)
+        {
+            int mid =(low+high)/2;
+            if(mid ==tg)
+            {
+                return mid;
+            }else if(arr[mid]<tg){
+                low = mid +1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+    static void Main()
+    {
+        int[] num = {10, 25, 30, 45, 60, 75};
+        int tg = 45;
+        int res = BinarySearch(num, tg);
+        if (res != -1)
+        {
+            Console.WriteLine($"Element {tg} found at Index {res}");
+        }
+        else
+        {
+            Console.WriteLine($"Element {tg} not found in the Array");
+        }
+    }
