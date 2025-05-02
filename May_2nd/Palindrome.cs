8099ed9559be2b6d62e00ae0d2696a4b4b849bf4
Thanooj Prapulla <thanoojprapulla@wipro>
2025-05-02 11:47:02 +0530

    Palindrome

--- May_2nd/Palindrome.cs
+++ May_2nd/Palindrome.cs
@@ -1,26 +1,30 @@
 using System;
 class Palindrome
-{
-    static void Main(string[] args)
+{static bool IsPalindrome()
     {
-        Console.WriteLine("Enter a string:");
-        string str = Console.ReadLine();
-        string reversedStr = ReverseString(str);
-        
-        if (str.Equals(reversedStr, StringComparison.OrdinalIgnoreCase))
+        string cleanedStr = string.Concat(str.where(char.IsLetterOrDigit)).ToLower();
+        int Left =0;
+        int Right = cleanedStr.Length -1;
+        while(Left<right)
         {
-            Console.WriteLine($"{str} is a palindrome.");
+            if(cleanedStr[Left]!= cleanedStr[Right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        if(IsPalindrome(input))
+        {
+            Console.WriteLine($"{input} is a palindrome.");
         }
         else
         {
-            Console.WriteLine($"{str} is not a palindrome.");
+            Console.WriteLine($"{input} is not a palindrome.");
         }
-    }
-
-    static string ReverseString(string str)
-    {
-        char[] charArray = str.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
     }
 }
