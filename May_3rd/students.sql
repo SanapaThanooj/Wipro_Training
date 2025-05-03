7df540bf52fa8621376dde8513f5c8180fa0f419
Thanooj Prapulla <sanapathanoojprapulla@gmail.com>
2025-05-03 14:15:14 +0530

    Students

--- May_3rd/students.sql
+++ May_3rd/students.sql
@@ -1,34 +1,42 @@
-/* INSERT INTO Students(StudentID, FirstName ,LastName, Age ,Department, Marks)
+Create Table Students(
+StudentID INT PRIMARY KEY,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Age INT,
+Department VARCHAR(50),
+Marks INT
+);
+INSERT INTO Students(StudentID, FirstName ,LastName, Age ,Department, Marks)
 VALUES
 (1,'Amit','sharam','21','Computer Science', 85),
 (2,'Neha','Kumar','22','Electronics', 75),
 (3,'Rahul','sharam','20','Mechanical', 66),
-(4,'Sneha','Singh','23','Computer Science', 91); */
-/* Select * From Students
-Where Department ='Computer Science'; */
-/* SELECT FirstName, Marks FROM Students
---WHERE Marks > 80; */
-/* UPDATE Students
+(4,'Sneha','Singh','23','Computer Science', 91); 
+ Select * From Students
+Where Department ='Computer Science'; 
+SELECT FirstName, Marks FROM Students
+WHERE Marks > 80; 
+ UPDATE Students
 SET Marks = 95
-Where StudentID = 2 */
-/* 
+Where StudentID = 2 
+ 
 UPDATE Students 
 Set Marks = 88
-Where StudentID in (1,3) */
-/*INSERT INTO Students(StudentID, FirstName, LastName, Age, Department, Marks)
+Where StudentID in (1,3) 
+NSERT INTO Students(StudentID, FirstName, LastName, Age, Department, Marks)
 VALUES
 (5,'kumar','varam','21','Mechanical', 92),
-(6,'sumanth','shetty','21','Electronics', 90); */
+(6,'sumanth','shetty','21','Electronics', 90); 
 --select * from Students
-/*UPDATE Students
+UPDATE Students
 SET Age = 24
-Where LastName = 'shetty'; */
-/*UPDATE Students
+Where LastName = 'shetty'; 
+UPDATE Students
 SET LastName = 'sanapa'
-Where FirstName = 'sumanth';*/
---select * from Students
---Delete From Students Where StudentID =1;
---DELETE FROM Students WHERE Marks < 88;
---DELETE FROM Students WHERE StudentID IN(3,5);
---TRUNCATE TABLE Students; -- data is removed but table is present
---Drop Table Students;--To remove table
+Where FirstName = 'sumanth';
+select * from Students
+Delete From Students Where StudentID =1;
+DELETE FROM Students WHERE Marks < 88;
+DELETE FROM Students WHERE StudentID IN(3,5);
+TRUNCATE TABLE Students; -- data is removed but table is present
+Drop Table Students;--To remove table
