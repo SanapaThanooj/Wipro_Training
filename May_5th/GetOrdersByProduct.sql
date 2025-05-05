77781111a25afb2230070e9d0dcc576316980604
Thanooj Prapulla <sanapathanoojprapulla@gmail.com>
2025-05-05 10:19:03 +0530

    GetOrdersProduct

---
+++ May_5th/GetOrdersByProduct.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE GetOrdersByProduct
+	@productID INT
+AS
+BEGIN
+	SELECT
+		o.Orderid,
+		o.Quantity,
+		o.OrderDate,
+		p.ProductName,
+		p.Price
+	FROM Orders o
+	INNER JOIN Products p ON o.productID = p.productID
+	WHERE o.productID = @productID;
+END;  
+
+EXEC GetOrdersByProduct @productID = 1;
--- May_5th/HigherPrice.sql
+++ May_5th/HigherPrice.sql
@@ -6,19 +6,3 @@ BEGIN
 END;
 
 EXEC HigherEndPriceS
-/*CREATE PROCEDURE GetOrdersByProduct
-	@productID INT
-AS
-BEGIN
-	SELECT
-		o.Orderid,
-		o.Quantity,
-		o.OrderDate
-		p.ProductName,
-		p.Price
-	FROM Orders o
-	INNER JOIN Products p ON o.productID = p.productID
-	WHERE o.productID = @productID;
-END;  */
---Msg 102, Level 15, State 1, Procedure GetOrdersByProduct, Line 9 [Batch Start Line 0] Incorrect syntax near '.'.
-
