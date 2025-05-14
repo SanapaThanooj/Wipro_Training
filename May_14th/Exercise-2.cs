824be70f2f727d08575da833e7e43855ac33ba7e
Thanooj Prapulla <sanapathanoojprapulla@gmail.com>
2025-05-14 10:27:58 +0530

    interface - Banking OPeration

--- May_14th/Exercise-2.cs
+++ May_14th/Exercise-2.cs
@@ -10,17 +10,27 @@ namespace Linq_Practices
     {
         void Deposit(decimal amount);
         void Withdraw(decimal amount);
-        public void CheckBalance();
+         void CheckBalance();
     }
      class SavingsAccount : IBankAccount 
     {
+        private decimal balance = 0;
         public void Deposit(decimal amount)
         {
+            balance += amount;
             Console.WriteLine($"Depositing {amount} into savings account.");
         }
         public void Withdraw(decimal amount)
         {
-            Console.WriteLine($"Withdrawing {amount} from savings account.");
+            if (amount > balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawing {amount} from savings account.");
+            }
         }
         public void CheckBalance()
         {
@@ -30,36 +40,48 @@ namespace Linq_Practices
     }
      class CurrentAccount : IBankAccount
     {
+        private decimal balance = 0;
         public void Deposit(decimal amount)
         {
+            balance += amount;
             Console.WriteLine($"Depositing {amount} into current account.");
         }
         public void Withdraw(decimal amount)
         {
-            Console.WriteLine($"Withdrawing {amount} from current account.");
+            if (amount > balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawing {amount} from current account.");
+            }
         }
         public void  CheckBalance()
         {
             Console.WriteLine("Checking balance in current account.");
-            
+           
         }
     }
-        class Program
+       
+}
+namespace Linq_Practices
+{
+    class Program
     {
         static void Main(string[] args)
         {
            IBankAccount savingAccount = new SavingsAccount();
             savingAccount.Deposit(1000);
             savingAccount.Withdraw(500);
-            Console.WriteLine($"Balance: {savingAccount.CheckBalance()}");
+            savingAccount.CheckBalance();
             Console.WriteLine();
             IBankAccount currentAccount = new CurrentAccount();
             currentAccount.Deposit(2000);
             currentAccount.Withdraw(1000);
-            Console.WriteLine($"Balance: {currentAccount.CheckBalance()}");
+            currentAccount.CheckBalance();
             Console.WriteLine();
         }
     }
-}
-
 }
