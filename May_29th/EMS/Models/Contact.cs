da204c8ef9c2089b1f69dc16d66f02f0c1e3a633
Thanooj Prapulla <sanapathanoojprapulla@gmail.com>
2025-05-29 13:53:09 +0530

    EMS

---
+++ May_29th/EMS/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Contact
+    {
+
+        public int Id { get; set; }
+        [Display(Name ="Full Name")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Email Address is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Subject is Required")]
+        [Display(Name = "Subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Message is Required")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
