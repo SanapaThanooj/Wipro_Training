744244365d8c81a5cde31b2e0e4c9a608fd3ba57
Thanooj Prapulla <sanapathanoojprapulla@gmail.com>
2025-06-03 10:59:10 +0530

    Assignment-1

---
+++ Jun_03/WebApplication11/WebApplication11/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication11.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string variant { get; set; }
+        public decimal Price { get; set; }
+       
+    }
+}
